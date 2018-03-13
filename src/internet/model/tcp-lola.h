584255c787fce5feb9c88551be6d51173e595e9a
ASWANTH P P <ppaswanth3@gmail.com>
2018-03-13 23:08:09 +0530

    lola function prototype added

--- src/internet/model/tcp-lola.h
+++ src/internet/model/tcp-lola.h
@@ -28,16 +28,46 @@ public:
    */
   TcpLola (const TcpLola& sock);
   virtual ~TcpLola (void);
-
   
   
+    virtual std::string GetName () const;
+   
+    
+  //Computes RTT needed to execute Lola algorithm
+  virtual void PktsAcked (Ptr<TcpSocketState> tcb, uint32_t segmentsAcked,
+                          const Time& rtt);
+//Enable/Disable Lola 
+ virtual void CongestionStateSet (Ptr<TcpSocketState> tcb,
 
 
+
+
+// Get slow start threshold for Lola 
+virtual uint32_t GetSsThresh (Ptr<const TcpSocketState> tcb,
+                                uint32_t bytesInFlight);
+// Increase congestion window in Cubic manner 
+virtual void cubicIncrease()
+protected:
 
 private:
+ //Enable Lola
+ void EnableLola (Ptr<TcpSocketState> tcb);
+
+  /**
+   * \brief Stop taking Vegas samples
+   */
+  void DisableLola ();
+  
+ private :
+ 
+ Time m_queueLow;    //Knob  minimum queue delay
+ Time m_queueTarget; //Knob 
+ Time m_minRtt;    // minimum value of RTT during meaurement time
+ Time m_maxRtt;    // maximum value of RTT during meaurement time
+ uint32_t m_cntRtt;                 //!< Number of RTT measurements during last RTT
+  bool m_doingLolaNow;              //!< If true, do Lola for this RTT
  
 
-protected:
   
 
 };
