da38194e7ecb88af466499710fc5ae0ddecdba63
joe019 <joeantony019@gmail.com>
2018-03-14 01:26:12 +0530

    minor edits

--- src/internet/model/tcp-lola.h
+++ src/internet/model/tcp-lola.h
@@ -27,42 +27,29 @@ public:
    * \param sock the object to copy
    */
   TcpLola (const TcpLola& sock);
+
   virtual ~TcpLola (void);
-  
-  
-    virtual std::string GetName () const;
-   
-    
+     
   //Computes RTT needed to execute Lola algorithm
   virtual void PktsAcked (Ptr<TcpSocketState> tcb, uint32_t segmentsAcked,
                           const Time& rtt);
-//Enable/Disable Lola 
- virtual void TcpLola::CongestionStateSet (Ptr<TcpSocketState> tcb,
+ 
+  virtual void TcpLola::CongestionStateSet (Ptr<TcpSocketState> tcb,
                               const TcpSocketState::TcpCongState_t newState);
 
-
-
-
-// Get slow start threshold for Lola 
-virtual uint32_t GetSsThresh (Ptr<const TcpSocketState> tcb,
+  // Get slow start threshold for Lola 
+  virtual uint32_t GetSsThresh (Ptr<const TcpSocketState> tcb,
                                 uint32_t bytesInFlight);
-// Increase congestion window in Cubic manner 
-void cubicIncrease();
 
+  // Increase congestion window in Cubic manner 
+  void cubicIncrease();
 
 private:
-
- 
- Time m_queueLow;    //Knob  minimum queue delay
- Time m_queueTarget; //Knob 
- Time m_minRtt;    // minimum value of RTT during meaurement time
- Time m_maxRtt;    // maximum value of RTT during meaurement time
- uint32_t m_cntRtt;                 //!< Number of RTT measurements during last RTT
-
- 
-
-  
-
+  Time m_queueLow;    //knob  minimum queue delay
+  Time m_queueTarget; //knob 
+  Time m_minRtt;    // minimum value of RTT during meaurement time
+  Time m_maxRtt;    // maximum value of RTT during meaurement time
+  uint32_t m_cntRtt;                 //!< Number of RTT measurements during last RTT
 };
 
 } // namespace ns3
